9c2a9b6bbb81f3555b0f8cb02e0dc7fde84a8554
Sherif Hamdy <sherif.hamdy.1995@gmail.com>
2018-05-11 19:48:58 +0200

    added cross origin on books controller

--- src/main/java/com/visual/shelf/demo/DemoApplication.java
+++ src/main/java/com/visual/shelf/demo/DemoApplication.java
@@ -11,10 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,16 +31,6 @@ public class DemoApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/book").allowedOrigins("http://localhost:8000");
-            }
-        };
     }
 
     @Override
--- src/main/java/com/visual/shelf/demo/controller/BookController.java
+++ src/main/java/com/visual/shelf/demo/controller/BookController.java
@@ -17,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/book")
+@CrossOrigin
 public class BookController {
 
     @Autowired
